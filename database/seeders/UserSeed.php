68eda247ce8b2e87f43ab708606c36e3da81b8cb
Onofre Souza <souzaonofre@gmail.com>
2024-02-07 17:12:15 -0300

    Implementando alim. dados de teste p/ Usuario.

---
+++ database/seeders/UserSeed.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'user@teste.com',
+            'password' =>  Hash::make('teste1234'),
+            'email_verified_at' => Date::now(config('app.timezone', 'America/Sao_Paulo'))
+        ]);
+    }
+}
