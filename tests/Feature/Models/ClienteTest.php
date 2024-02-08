551e882c1b9dec314fb5720e722774dfcaffed8a
Onofre Souza <souzaonofre@gmail.com>
2024-02-08 10:45:12 -0300

    Correcao titulo test model cliente.

--- tests/Feature/Models/ClienteTest.php
+++ tests/Feature/Models/ClienteTest.php
@@ -2,14 +2,25 @@
 
 use App\Models\Cliente;
 
-test('criar e verificar dados vendedor', function () {
-    $cliente = Cliente::query()->create([
-        'nome' => fake()->name(),
-        'telefone' => fake()->unique()->phoneNumber(),
-        'email' => fake()->unique()->safeEmail(),
-    ]);
+beforeEach(function () {
+    $this->clienteData = [
+        'nome' => 'Cliente1 Test',
+        'email' => 'client1@teste.com',
+        'telefone' => fake()->unique()->phoneNumber()
+    ];
+    $this->cliente = Cliente::query()->create($this->clienteData);
+});
 
-    expect($cliente)->toBeInstanceOf(Cliente::class);
+test('criar e verificar dados cliente', function () {
 
-    expect($cliente->id)->toBeInt();
+    expect($this->cliente)->toBeInstanceOf(Cliente::class);
+
+    expect($this->cliente->nome)->toBeString();
+    expect($this->cliente->nome)->toEqual($this->clienteData['nome']);
+
+    expect($this->cliente->email)->toBeString();
+    expect($this->cliente->email)->toEqual($this->clienteData['email']);
+
+    expect($this->cliente->telefone)->toBeString();
+    expect($this->cliente->telefone)->toEqual($this->clienteData['telefone']);
 });
